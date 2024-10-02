ae0b1b70c4dd095d5f3f988b322f72c7fd6770ba
Manikandan-Rangarajan <116093831+Manikandan-Rangarajan@users.noreply.github.com>
2024-10-02 14:03:15 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/expense');
+    await mongoose.connect('Enter your Mongo URI');
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
